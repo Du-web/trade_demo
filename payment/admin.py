46b26aa4b01adcdd52242e344b3d637a0d8fec0c
dudu <648302938@qq.com>
2020-12-02 15:38:59 +0800

    实现admin功能

--- payment/admin.py
+++ payment/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
+from payment.models import User, Accout, Trade
+
+
+class TradeAdmin(admin.ModelAdmin):
+    list_display = ['accout', 'user', 'trade_amount', 'trade_date', 'status', 'is_exceed_200']
+
+    def is_exceed_200(self, obj):
+        if obj.trade_amount >= 200:
+            return 'yes'
+        elif obj.trade_amount <= 200:
+            return 'no'
+
+    def trade_amount(self, obj):
+        trade_amount = obj.trade_amount
+
+        return trade_amount + 'RMB'
+
+    trade_amount.short_description = '交易金额'
+
+
+admin.site.register(User)
+admin.site.register(Accout)
+admin.site.register(Trade, TradeAdmin)
